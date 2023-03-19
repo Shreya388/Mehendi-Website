58e7fa5c05b1267cd830f9f9d375e318d0bc038d
Shreya388 <meenaxjha@gmail.com>
2023-03-19 09:47:37 +0530

    Resized Images

---
+++ images/about/main.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+# Set the desired width and height
+width = 616
+height = 902
+
+# Loop through all files in the current directory
+for filename in os.listdir('.'):
+    # Check if the file is an image
+    if filename.endswith('.jpg') or filename.endswith('.webp'):
+        # Open the image using Pillow
+        with Image.open(filename) as img:
+            # Resize the image
+            img = img.resize((width, height))
+            # Save the resized image with a new filename
+            img.save('./Resized/' + filename)
